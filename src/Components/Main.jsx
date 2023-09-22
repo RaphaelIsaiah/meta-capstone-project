6af7e808c7f84bb6dc73e977b771a0bd83ce34e9
RaphaelIsaiah <rafvie12@gmail.com>
2023-09-22 05:09:09 +0100

    Hero section corrected and responsive.

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -15,24 +15,30 @@ export default function Main() {
     <main className="Main-container">
       <section className="Main-hero">
         {/* This section is partly done... Proper alignment and mobile responsiveness are pending. */}
-        <div className="Hero-bcgd">
-          <img
-            src={hero}
-            alt="Chef with food"
-            className="Hero-img"
-            width={328}
-            height={360}
-          />
+        <div className="Hero-closure">
+          <div className="H-txt-btn">
+            <p className="Hero-ll">Little Lemon</p>
+            <p className="Hero-ch">Chicago</p>
+            <p className="Hero-des">
+              We are a family owned Mediterranean restaurant, focused on
+              traditional recipes served with a modern twist.
+            </p>
+            <div className="btn-hold">
+              <button className="LL-btn">
+                <a href="#">Reserve a Table</a>
+              </button>
+            </div>
+          </div>
+          <div className="Hero-img">
+            <img
+              src={hero}
+              alt="Chef with food"
+              className="chef-n-food"
+              width={328}
+              height={360}
+            />
+          </div>
           {/* I'm having a problem styling this image to the design and also making it responsive */}
-          <p className="Hero-ll">Little Lemon</p>
-          <p className="Hero-ch">Chicago</p>
-          <p className="Hero-des">
-            We are a family owned Mediterranean restaurant, focused on
-            traditional recipes served with a modern twist.
-          </p>
-          <button className="LL-btn">
-            <a href="#">Reserve a Table</a>
-          </button>
         </div>
       </section>
       {/* ----This Hero section needs to be reviewed and styled properly for a more responsive look.---- */}
